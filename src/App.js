e59eaffaec1d4228476b1e70ac14c4b169eaefb4
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2022-10-16 20:41:06 +0100

    adjust main div

--- src/App.js
+++ src/App.js
@@ -10,23 +10,5 @@ function App() {
   )
 }
 
-// const Header = () => {
-//   return (
-//     <div className='header'>
-//       <logo className='logo'>
-//         <img src={logo} alt='' srcset='' />
-//       </logo>
-//       <div className='nav'>
-//         <ul>
-//           <li>Home</li>
-//           <li>Products</li>
-//           <li>Community</li>
-//           <li>Contact</li>
-//         </ul>
-//       </div>
-//       <div></div>
-//     </div>
-//   )
-// }
 
 export default App
