a104d8e0eab6f11fc3c990fea00ebb20bf5d5fe2
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2022-10-17 10:57:30 +0100

    add animation to can for test

--- src/Content.js
+++ src/Content.js
@@ -4,13 +4,24 @@ import can from './imgs/can.png'
 const Content = () => {
   return (
     <div className='container'>
-     <div className='text'>
-      <h1>BE YOUNG, HAVE FUN,<br/><span>DRINK PEPSI</span></h1>
-      <p>Pepsi is a carbonated soft drink manufactured by PepsiCo.<br/>Originally created and developed in 1893 by Caleb Bradham and introduced as Brad's Drink, it was renamed as Pepsi-<br/>Cola in 1898, and then shortended to Pepsi in 1961.</p>
-     </div>
-     <div className='can'>
-      <img src={can} alt="" srcset="" />
-     </div>
+      <div className='text'>
+        <h1>
+          BE YOUNG, HAVE FUN,
+          <br />
+          <span>DRINK PEPSI</span>
+        </h1>
+        <p>
+          Pepsi is a carbonated soft drink manufactured by PepsiCo.
+          <br />
+          Originally created and developed in 1893 by Caleb Bradham and
+          introduced as Brad's Drink, it was renamed as Pepsi-
+          <br />
+          Cola in 1898, and then shortended to Pepsi in 1961.
+        </p>
+      </div>
+      <div className='App-logo'>
+        <img src={can} alt='' srcset='' />
+      </div>
     </div>
   )
 }
