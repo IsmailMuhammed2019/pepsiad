ca5ed4d917e1e1b70b9d8cc251cbf1273faab730
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2022-10-17 11:45:47 +0100

    add buy button

--- src/Content.js
+++ src/Content.js
@@ -4,20 +4,23 @@ import can from './imgs/can.png'
 const Content = () => {
   return (
     <div className='container'>
-      <div className='text'>
-        <h1>
-          BE YOUNG, HAVE FUN,
-          <br />
-          <span className='big'>DRINK PEPSI</span>
-        </h1>
-        <p className='textcontent'>
-          Pepsi is a carbonated soft drink manufactured by PepsiCo.
-          <br />
-          Originally created and developed in 1893 by Caleb Bradham <br/>and
-          introduced as Brad's Drink, it was renamed as Pepsi-
-          <br />
-          Cola in 1898, and then shortended to Pepsi in 1961.
-        </p>
+      <div>
+        <div className='text'>
+          <h1>
+            BE YOUNG, HAVE FUN,
+            <br />
+            <span className='big'>DRINK PEPSI</span>
+          </h1>
+          <p className='textcontent'>
+            Pepsi is a carbonated soft drink manufactured by PepsiCo.
+            <br />
+            Originally created and developed in 1893 by Caleb Bradham <br />
+            and introduced as Brad's Drink, it was renamed as Pepsi-
+            <br />
+            Cola in 1898, and then shortended to Pepsi in 1961.
+          </p>
+        </div>
+        <button className='btn grey bottom'>BUY NOW</button>
       </div>
       <div className='App-logo'>
         <img src={can} alt='' srcset='' />
