3b42d3f0aeba836251a5851091ecc7056db8b1c0
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2022-10-17 10:54:44 +0100

    add content text

--- src/Content.js
+++ src/Content.js
@@ -5,7 +5,8 @@ const Content = () => {
   return (
     <div className='container'>
      <div className='text'>
-
+      <h1>BE YOUNG, HAVE FUN,<br/><span>DRINK PEPSI</span></h1>
+      <p>Pepsi is a carbonated soft drink manufactured by PepsiCo.<br/>Originally created and developed in 1893 by Caleb Bradham and introduced as Brad's Drink, it was renamed as Pepsi-<br/>Cola in 1898, and then shortended to Pepsi in 1961.</p>
      </div>
      <div className='can'>
       <img src={can} alt="" srcset="" />
