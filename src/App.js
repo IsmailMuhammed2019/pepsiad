c05b11e58a26347054fb533b59731131d7bdee26
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2022-10-17 10:34:33 +0100

    add can image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './Header'
+import Content from './Content'
 import './App.css'
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <Content />
     </div>
   )
 }
---
+++ src/Content.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import can from './imgs/can.png'
+
+const Content = () => {
+  return (
+    <div className='container'>
+     <div className='text'>
+
+     </div>
+     <div className='can'>
+      <img src={can} alt="" srcset="" />
+     </div>
+    </div>
+  )
+}
+
+export default Content
